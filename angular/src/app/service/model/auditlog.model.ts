158eaddb3fa9297369abd07eac3489e5a1e6a39d
minh.nguyenvan <minh.nguyenvan@ncc.asia>
2023-09-25 09:17:35 +0700

    updatecode

---
+++ angular/src/app/service/model/auditlog.model.ts
@@ -0,0 +1,20 @@
+export class AuditlogDto {
+    ExecutionDuration: number;
+    ExecutionTime: string;
+    MethodName: string;
+    Parameters: string;
+    ServiceName: string;
+    UserId: number;
+    Note: string;
+    UserIdString: string;
+    EmailAddress: string;
+    hideNote: boolean;
+}
+export class EmailAddressInAuditLog {
+    UserId: number;
+    EmailAddress: string;
+}
+export interface LoginConfigDto {
+    googleClientId: string
+    enableNormalLogin: boolean
+}
